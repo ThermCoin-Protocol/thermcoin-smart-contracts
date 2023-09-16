15a8378b09a63924b349835c2c1fae276ce87749
CarterBloop <carter@tiogaventures.com>
2023-09-16 15:59:33 -0700

    Smart Contract Restructuring

--- contracts/ThermCoin.sol
+++ contracts/ThermCoin.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.9;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
-import "@openzeppelin/contracts/utils/cryptography/ECDSA.sol";
+import "./ERC20Rebasable.sol";
 
 // import console
 import "hardhat/console.sol";
 
-contract ThermCoin is ERC20, ERC20Burnable, Ownable {
+contract ThermCoin is ERC20Rebasable, Ownable {
     // Tokenomics
     uint256 private _baseFee; // Transaction fee (in wei)
     uint256 private _feeIncrement; // Fee increment (in wei)
@@ -29,15 +29,60 @@ contract ThermCoin is ERC20, ERC20Burnable, Ownable {
         uint256 baseFee,
         uint256 feeIncrement,
         uint256 volumeThreshold
-    ) ERC20("ThermCoin", "BTUC") {
+    ) ERC20Rebasable("ThermCoin", "BTUC") {
         _mint(msg.sender, premintAmt * 10 ** decimals());
-        _updateHolder(msg.sender);
         _lastBlockNum = block.number;
         _baseFee = baseFee;
         _feeIncrement = feeIncrement;
         _volumeThreshold = volumeThreshold;
         _currentTxVolume = 0;
         _prevTxVolume = 0;
+    }
+
+    // Change each account's balance to match the new total supply
+    function rebase(uint256 percentageChange) public onlyOwner {
+        _rebase(percentageChange);
+    }
+
+    function mint(address to, uint256 amount) public onlyOwner {
+        _mint(to, amount);
+    }
+
+    /* -------------------------- */
+    /* Transfer and Tx Fee helper */
+    /* -------------------------- */
+
+    function setFeeParams(
+        uint256 newBase,
+        uint256 newIncrement,
+        uint256 newThreshold
+    ) public onlyOwner {
+        _baseFee = newBase;
+        _feeIncrement = newIncrement;
+        _volumeThreshold = newThreshold;
+    }
+
+    function getFeeParams() public view returns (uint256, uint256, uint256) {
+        return (_baseFee, _feeIncrement, _volumeThreshold);
+    }
+
+    function _calculateTxFee() private returns (uint256) {
+        uint256 fee = getTxFee();
+        _currentTxVolume++;
+        if (_lastBlockNum != block.number) {
+            _prevTxVolume = _currentTxVolume;
+            _currentTxVolume = 0;
+            _lastBlockNum = block.number;
+        }
+        return fee;
+    }
+
+    // Fee increases based on tx volume
+    // Fee starts at the base fee
+    // Increases by the fee increment every n transactions
+    function getTxFee() public view returns (uint256) {
+        return (_baseFee +
+            (_feeIncrement * (_prevTxVolume / _volumeThreshold)));
     }
 
     /* -------------------------- */
@@ -85,153 +130,5 @@ contract ThermCoin is ERC20, ERC20Burnable, Ownable {
         _transfer(from, to, amount - fee);
         // Transfer the fee from the sender to the relayer (the sender of this transaction).
         _transfer(from, msg.sender, fee);
-    }
-
-    /* ------------------------ */
-    /* Administrative functions */
-    /* ------------------------ */
-
-    // Mint tokens to a wallet (Call this function in ether units)
-    function mint(address to, uint256 amount) public onlyOwner {
-        _mint(to, amount * 10 ** decimals());
-        _updateHolder(to);
-    }
-
-    /* ---------------- */
-    /* Holder functions */
-    /* ---------------- */
-
-    struct Holder {
-        bool exists;
-        uint256 index;
-    }
-
-    mapping(address => Holder) private _holderData; // For efficient lookup
-    address[] private _holders; // Tracks all token holders
-
-    // Add or remove wallet from holders list
-    function _updateHolder(address wallet) private {
-        if (balanceOf(wallet) > 0 && !_holderData[wallet].exists) {
-            _holderData[wallet] = Holder({
-                exists: true,
-                index: _holders.length
-            });
-            _holders.push(wallet);
-        } else if (balanceOf(wallet) == 0 && _holderData[wallet].exists) {
-            address lastHolder = _holders[_holders.length - 1];
-
-            uint256 removedHolderIndex = _holderData[wallet].index;
-            _holders[removedHolderIndex] = lastHolder;
-            _holderData[lastHolder].index = removedHolderIndex;
-
-            _holders.pop();
-            delete _holderData[wallet];
-        }
-    }
-
-    // Returns all accounts with token balance
-    function getHolders() public view returns (address[] memory) {
-        return _holders;
-    }
-
-    /* --------------- */
-    /* Rebase Function */
-    /* --------------- */
-
-    // Change each account's balance to match the new total supply
-    function rebaseAllAccounts(
-        uint256 supplyDelta,
-        bool increase
-    ) public onlyOwner returns (uint256 delta) {
-        uint256 totalSupply = totalSupply();
-        supplyDelta = supplyDelta * 10 ** decimals();
-        for (uint256 i = 0; i < _holders.length; i++) {
-            address holder = _holders[i];
-            uint256 holderBalance = balanceOf(holder);
-            uint256 holderDelta = holderBalance.mul(supplyDelta).div(
-                totalSupply
-            );
-            if (increase) {
-                _mint(holder, holderDelta);
-            } else {
-                _burn(holder, holderDelta);
-            }
-        }
-
-        uint256 newSupply = super.totalSupply();
-        return newSupply;
-    }
-
-    /* -------------------------- */
-    /* Transfer and Tx Fee helper */
-    /* -------------------------- */
-
-    function setFeeParams(
-        uint256 newBase,
-        uint256 newIncrement,
-        uint256 newThreshold
-    ) public onlyOwner {
-        _baseFee = newBase;
-        _feeIncrement = newIncrement;
-        _volumeThreshold = newThreshold;
-    }
-
-    function getFeeParams() public view returns (uint256, uint256, uint256) {
-        return (_baseFee, _feeIncrement, _volumeThreshold);
-    }
-
-    function _calculateTxFee() private returns (uint256) {
-        uint256 fee = getTxFee();
-        _currentTxVolume++;
-        if (_lastBlockNum != block.number) {
-            _prevTxVolume = _currentTxVolume;
-            _currentTxVolume = 0;
-            _lastBlockNum = block.number;
-        }
-        return fee;
-    }
-
-    // Fee increases based on tx volume, fee factors, and fiboncacci sequence
-    function getTxFee() public view returns (uint256) {
-        if (_prevTxVolume < _volumeThreshold) {
-            return (_baseFee);
-        } else if (_prevTxVolume < 2 * _volumeThreshold) {
-            return (_baseFee + _feeIncrement);
-        } else if (_prevTxVolume < 3 * _volumeThreshold) {
-            return (_baseFee + (_feeIncrement * 2));
-        } else if (_prevTxVolume < 5 * _volumeThreshold) {
-            return (_baseFee + (_feeIncrement * 3));
-        } else if (_prevTxVolume < 8 * _volumeThreshold) {
-            return (_baseFee + (_feeIncrement * 4));
-        } else if (_prevTxVolume < 13 * _volumeThreshold) {
-            return (_baseFee + (_feeIncrement * 5));
-        } else if (_prevTxVolume < 21 * _volumeThreshold) {
-            return (_baseFee + (_feeIncrement * 6));
-        } else {
-            return (_baseFee + (_feeIncrement * 7));
-        }
-    }
-
-    function transfer(
-        address to,
-        uint256 amount
-    ) public override returns (bool) {
-        bool txStatus = super.transfer(to, amount); // Transfer tokens
-        // Update holders
-        _updateHolder(to);
-        _updateHolder(msg.sender);
-        return txStatus;
-    }
-
-    function transferFrom(
-        address from,
-        address to,
-        uint256 amount
-    ) public override returns (bool) {
-        bool txStatus = super.transferFrom(from, to, amount); // Transfer tokens
-        // Update holders
-        _updateHolder(to);
-        _updateHolder(from);
-        return txStatus;
     }
 }
